8faa16e9551906322f88cc5e0b2cbfa47536fd52
Ashfaq Shaik <Ashfaq.Shaik@broadridge.com>
2017-04-24 20:30:32 +0530

    Initial Commit

---
+++ buildQuorumImage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Running docker build with proxy param : $1"
+
+docker build    --build-arg PROXY=$1                        \
+                -f ./Docker/dockerfile                      \
+                -t broadridge/quorum:1.1.0 .
+                 
